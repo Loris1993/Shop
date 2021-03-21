d2d53bb135a427bc743b652681cffece3e3db9e8
Loris <loris.1993@gmail.com>
2021-03-21 21:33:42 +0100

    add @service

--- src/main/java/com/shop/webapp/service/ArticoliServiceImpl.java
+++ src/main/java/com/shop/webapp/service/ArticoliServiceImpl.java
@@ -1,5 +1,54 @@
 package com.shop.webapp.service;
 
-public class ArticoliServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.shop.webapp.entity.Articoli;
+import com.shop.webapp.repostiory.ArticoliRepository;
+
+@Service
+@Transactional(readOnly = true)
+public class ArticoliServiceImpl implements ArticoliService{
+
+	@Autowired
+	ArticoliRepository articoliRepository;
+
+	@Override
+	public List<Articoli> SelByDescrizione(String descrizione) 
+	{
+		return articoliRepository.findByDescrizioneLike(descrizione.toUpperCase());
+	}
+
+	@Override
+	public Articoli SelByCodArt(String codArt) 
+	{
+		 
+		return articoliRepository.findByCodArt(codArt);
+	}
+
+	@Override
+	public Articoli SelByBarcode(String barcode) 
+	{
+		
+		return articoliRepository.SelByEan(barcode);
+	}
+
+	@Override
+	@Transactional
+	public void DelArticolo(Articoli articolo) 
+	{
+		articoliRepository.delete(articolo);
+	}
+
+	@Override
+	@Transactional
+	public void InsArticolo(Articoli articolo) 
+	{
+		articoliRepository.save(articolo);
+	}
+
+	
+	
 }
