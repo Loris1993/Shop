7c579f7abfba5777fc49dc76166ef274e7987c88
Loris <loris.1993@gmail.com>
2021-03-21 23:00:57 +0100

    creazione articoliController

--- src/main/java/com/shop/webapp/service/ArticoliServiceImpl.java
+++ src/main/java/com/shop/webapp/service/ArticoliServiceImpl.java
@@ -3,6 +3,7 @@ package com.shop.webapp.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.shop.webapp.entity.Articoli;
